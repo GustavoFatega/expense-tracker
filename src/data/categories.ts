94cce4933cd54c3096abab5b166c61431ddda36e
Gustavo Fatega <gus.fatega@gmail.com.br>
2022-07-25 17:24:08 -0300

    Projeto finalizado

---
+++ src/data/categories.ts
@@ -0,0 +1,19 @@
+import { Category} from '../types/category'
+
+export const categories: Category = {
+  food: { 
+    title: 'alimentação', 
+    color: 'blue', 
+    expense: true
+  },
+  rend: {
+    title: 'aluguel', 
+    color: 'brown', 
+    expense: true 
+  },
+  salary: {
+    title: 'Salário', 
+    color: 'green', 
+    expense: false 
+  },
+}
